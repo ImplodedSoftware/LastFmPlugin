a113d13abedecffb107459e34daac38558882320
Mikael Stalvik <mikael@helium.fm>
2016-07-03 14:19:02 +0200

    Updated JSON.NET

--- Helpers/LastFmConstants.cs
+++ Helpers/LastFmConstants.cs
@@ -2,8 +2,8 @@ namespace LastFmPlugin.Helpers
 {
     public class LastFmConstants
     {
-        public const string API_KEY = "<insert_key>";
-        public const string API_SECRET = "<insert_secret>";
+public const string API_KEY = "<insert_key>";
+public const string API_SECRET = "<insert_secret>";
 
         public const string URL_GET_ARTIST_INFO_BY_NAME =
             "http://ws.audioscrobbler.com/2.0/?method=artist.getinfo&artist={0}&api_key=" + API_KEY + "&format=json";
